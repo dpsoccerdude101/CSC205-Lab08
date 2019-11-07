c059cce72c9234e127f63691be253ea1f28b2145
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2019-11-07 14:22:36 -0500

    Update Inventory.java

--- Inventory.java
+++ Inventory.java
@@ -8,15 +8,17 @@ import java.util.*;
 
 public class Inventory {
 
-	//instance variables
+	// instance variables
 	private ArrayList<Part> inventoryList = new ArrayList<Part>();
-	
+
 	/**
 	 * method that alphabetizes the parts
+	 * 
 	 * @param p
 	 */
 	public void sortedInsert(Part p) {
-		//failed attempt and insertion sort and many others, array[] should be replaced with inventoryList
+		// failed attempt and insertion sort and many others, array[] should be replaced
+		// with inventoryList
 //		for (int i = 1; i < inventoryList.size(); i++) {
 //	        Part current = inventoryList.get(i);
 //	        int j = i - 1;
@@ -35,77 +37,98 @@ public class Inventory {
 //			
 //				
 //			}
-		//}
-	    
-	            }
-	   
-	    /**
-	     * Method that returns the total values of all the parts in the arraylist
-	     * @return double
-	     */
-		public double totalInventoryValue() {
-			double total = 0;
-			for(int i = 0; i < inventoryList.size(); i++) {
-				total += inventoryList.get(i).getValue();
-			}
-			return total;
+		// }
+
+	}
+
+	/**
+	 * Method that returns the total values of all the parts in the arraylist
+	 * 
+	 * @return double
+	 */
+	public double totalInventoryValue() {
+		double total = 0;
+		for (int i = 0; i < inventoryList.size(); i++) {
+			total += inventoryList.get(i).getValue();
 		}
-		
-		/**
-		 * Method that returns all parts that match the color
-		 * @param color
-		 * @return string
-		 */
-		public String searchByColor(String color) {
-			String concatInventoryMatches = "";
-			for(int i = 0; i < inventoryList.size(); i++) {
-				if(inventoryList.get(i).getColor().equalsIgnoreCase(color)) {
-					concatInventoryMatches += (inventoryList.get(i).toString() + "\n");
+		return total;
+	}
+
+	/**
+	 * Method that returns all parts that match the color
+	 * 
+	 * @param color
+	 * @return string
+	 */
+	public String searchByColor(String color) {
+		String concatInventoryMatches = "";
+		for (int i = 0; i < inventoryList.size(); i++) {
+			if (inventoryList.get(i).getColor().equalsIgnoreCase(color)) {
+				concatInventoryMatches += (inventoryList.get(i).toString() + "\n");
+			}
+		}
+		return concatInventoryMatches;
+	}
+
+	/**
+	 * Returns the heaviest part in the arrayList
+	 * 
+	 * @return string
+	 */
+	public String heaviestPart() {
+		Part heaviest = inventoryList.get(0);
+
+		for (int i = 0; i < inventoryList.size(); i++) {
+			if (inventoryList.get(i).getWeight() > heaviest.getWeight()) {
+				heaviest = inventoryList.get(i);
+			}
+		}
+		return heaviest.toString();
+	}
+
+	/**
+	 * method returns the total of the amount in stock of the given part
+	 * 
+	 * @param partName
+	 * @return int
+	 */
+	public int numPartsInStock(String partName) {
+		int total = 0;
+		int l = 0;
+		int listSize = inventoryList.size() - 1;
+		while (l <= listSize) {
+			int mid = 1 + (listSize - 1) / 2;
+
+			if (inventoryList.get(mid).getPartName().equalsIgnoreCase(partName)) {
+				int count = mid;
+				while (inventoryList.get(count).getPartName().equalsIgnoreCase(partName)) {
+					count--;
+				}
+				while (inventoryList.get(count).getPartName().equalsIgnoreCase(partName)) {
+				total += inventoryList.get(count).getAmountInStock();
+				count++;
+				return total;
 				}
 			}
-			return concatInventoryMatches;
-		}
-		
-		/**
-		 * Returns the heaviest part in the arrayList
-		 * @return string
-		 */
-		public String heaviestPart() {
-			Part heaviest = inventoryList.get(0);
+			if (inventoryList.get(mid).getPartName().compareTo(partName) < 0) 
+				l = mid + 1;
 			
-			for(int i = 0; i < inventoryList.size(); i++) {
-				if (inventoryList.get(i).getWeight() > heaviest.getWeight()){
-					heaviest = inventoryList.get(i);
-				}
-			}
-			return heaviest.toString();
-		}
-		
-		/**
-		 * method returns the total of the amount in stock of the given part
-		 * @param partName
-		 * @return int
-		 */
-		public int numPartsInStock(String partName) {
-			int total = 0;
-			for(int i = 0; i < inventoryList.size(); i++) {
-				if(inventoryList.get(i).getPartName() == partName) {
-					total += inventoryList.get(i).getAmountInStock();
-				}
-			}
-			return total;
-		}
-		
-		public ArrayList<Part> reOrderList(){
+			else 
+				listSize = mid - 1;
 			
-			ArrayList<Part> reOrderedList = new ArrayList<Part>();
-			for(int i = 0; i < inventoryList.size(); i++) {
-				if(inventoryList.get(i).getAmountInStock() < inventoryList.get(i).getReorderPoint()) {
-					reOrderedList.add(inventoryList.get(i));
-				}
 			}
-			return reOrderedList;
+		return 0;
 		}
-	
+
+	public ArrayList<Part> reOrderList() {
+
+		ArrayList<Part> reOrderedList = new ArrayList<Part>();
+		for (int i = 0; i < inventoryList.size(); i++) {
+			if (inventoryList.get(i).getAmountInStock() < inventoryList.get(i).getReorderPoint()) {
+				reOrderedList.add(inventoryList.get(i));
+			}
+		}
+		return reOrderedList;
+	}
 
 }
