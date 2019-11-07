58dbafed9efdebf8318a1591f452d6c9a824d177
Dennis Pavlyuk <37779084+dpsoccerdude101@users.noreply.github.com>
2019-11-07 13:35:01 -0500

    Update Part.java

--- Part.java
+++ Part.java
@@ -24,39 +24,38 @@ public class Part {
 	 * @param amountInStock
 	 * @param reorderPaint
 	 */
+	public Part(String pN, String c, double wt, double pr, int amtInStk, int reOrderPt) {
+		partName = pN;
+		color = c;
 
-	public Part(String partName, String color, double weight, double price, int amountInStock, int reorderPaint) {
-		super();
-		this.partName = partName;
-		this.color = color;
-		this.weight = weight;
-		this.price = price;
-		this.amountInStock = amountInStock;
-		this.reorderPoint = reorderPaint;
+		weight = isWeightReal(wt) ? wt : 0.0;
+		price = isPriceReal(pr) ? pr : 0.0;
+		amountInStock = isAmountInStockReal(amtInStk) ? amtInStk : 0;
+		reorderPoint = isReOrderPointReal(reOrderPt) ? reOrderPt : 0;
 	}
-		/**
-		 * Method that compares two parts by name then weight 
-		 * to see what one is 
-		 * 
-		 * @param other
-		 * @return int
-		 */
+
+	/**
+	 * Method that compares two parts by name then weight to see what one is
+	 * 
+	 * @param other
+	 * @return int
+	 */
 	public int compareTo(Part other) {
 		int part = 0;
 		if (this.partName.contentEquals(other.getPartName())) {
 			if (this.weight < other.getWeight()) {
 				part = -1;
 			} else if (this.weight == other.getWeight()) {
-					part = 0;
-				} else if (this.weight > other.getWeight()) {
-					part = 1;
-				}
-		} else if(this.partName.compareTo(other.getPartName()) < 0) {
-				part = -1;
-			} else if(this.partName.compareTo(other.getPartName()) > 0) {
+				part = 0;
+			} else if (this.weight > other.getWeight()) {
 				part = 1;
 			}
-			return part;
+		} else if (this.partName.compareTo(other.getPartName()) < 0) {
+			part = -1;
+		} else if (this.partName.compareTo(other.getPartName()) > 0) {
+			part = 1;
+		}
+		return part;
 	}
 
 	/**
@@ -85,17 +84,33 @@ public class Part {
 	public int getReorderPoint() {
 		return reorderPoint;
 	}
+
 	/**
-	 * Method that returns the value of price
-	 * multiplied by amountInStock 
+	 * Method that returns the value of price multiplied by amountInStock
 	 */
 	public double getValue() {
 		return this.price * this.amountInStock;
 	}
-	
+
 	public String toString() {
-		return ("Part name: " + this.partName + "\n" + "Part color: " + this.color + "\n" + "Part weight "
-				+ this.weight + "\n" + "Part price: " + this.price + "\n" + "Amount in stock: "
-				+ this.amountInStock + "\n" + "Reorder point: " + this.reorderPoint + "\n");
+		return ("Part name: " + this.partName + "\n" + "Part color: " + this.color + "\n" + "Part weight " + this.weight
+				+ "\n" + "Part price: " + this.price + "\n" + "Amount in stock: " + this.amountInStock + "\n"
+				+ "Reorder point: " + this.reorderPoint + "\n");
+	}
+
+	private boolean isWeightReal(double wt) {
+		return (wt > 0.0);
+	}
+
+	private boolean isPriceReal(double pr) {
+		return (pr > 0.0);
+	}
+
+	private boolean isAmountInStockReal(double amtInStk) {
+		return (amtInStk >= 0);
+	}
+
+	private boolean isReOrderPointReal(int reOrderPt) {
+		return (reOrderPt > 0);
 	}
 }
